b0125294514db5cadb82a9aa895f6eb29dcb50e4
Ryan Ramadhani <Ryan Ramadhani@DESKTOP-N31T9G9>
2018-08-14 06:13:39 +0700

    Coba method substring, replace, & split

--- src/Praktek/CobaOperasiString.java
+++ src/Praktek/CobaOperasiString.java
@@ -65,5 +65,25 @@ public class CobaOperasiString {
         
         System.out.println("Gabungkan isi variable x dan r : " + x.concat(r));
         
+        System.out.println("Karakter mulai posisi 3 di variable x : " 
+                + x.substring(3));
+        
+        System.out.println("Karakter dari posisi 3-5 di variable x : " 
+                + x.substring(3,6));
+        
+        String tclean = t.trim();
+        System.out.println("Isi variable tclean : " + t.trim());
+        System.out.println("Rubah 'i' jadi 'E' di variable tclean : " 
+                + tclean.replace("i","E"));
+        
+        String[] arrT = tclean.split(" ");
+        System.out.println("Pecah kata di variable tclean : " );
+        for (int i=0; i<arrT.length; i++)
+            System.out.println(arrT[i]);
+        
+        String[] arrTr = tclean.split("r");
+        System.out.println("Pecah berdasarkan huruf 'r' di variable tclean : ");
+        for (int i=0; i<arrTr.length; i++)
+            System.out.println(arrTr[i]);
     }
 }
