f9875d3dd7c2e386726709d35a130711a7e3b8bf
Ryan Ramadhani <Ryan Ramadhani@DESKTOP-N31T9G9>
2018-08-14 06:02:47 +0700

    Initial Commit

---
+++ src/Praktek/CobaOperasiString.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktek;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Ryan Ramadhani
+ */
+public class CobaOperasiString {
+    public static void main(String[] args)
+    {
+        String identitas = "Ryan Ramadhani / X RPL 2 / 29";
+        System.out.println("Identitas : " + identitas);
+        
+    }
+}
